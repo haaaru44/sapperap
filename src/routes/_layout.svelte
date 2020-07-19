477dd0512b968d4b1c77156641bd8161041f2f13
haaaru55 <haaaru5500@gmail.com>
2020-07-19 23:37:14 +0900

    アナリティクス

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -1,22 +1,28 @@
 <script>
-	import Nav from '../components/Nav.svelte';
+  import Nav from '../components/Nav.svelte'
 
-	export let segment;
+  import GoogleAnalytics from 'sapper-google-analytics/GoogleAnalytics.svelte'
+  import { stores } from '@sapper/app'
+
+  let ga_measurment_id = 'UA-172142370-1'
+  // your analytics id
+  export let segment
 </script>
 
 <style>
-	main {
-		position: relative;
-		max-width: 56em;
-		background-color: white;
-		padding: 2em;
-		margin: 0 auto;
-		box-sizing: border-box;
-	}
+  main {
+    position: relative;
+    max-width: 56em;
+    background-color: white;
+    padding: 2em;
+    margin: 0 auto;
+    box-sizing: border-box;
+  }
 </style>
 
-<Nav {segment}/>
+<GoogleAnalytics {stores} id={ga_measurment_id} />
+<Nav {segment} />
 
 <main>
-	<slot></slot>
+  <slot />
 </main>
